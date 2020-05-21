774c50d7a8f2575cfefded211d51275db9d83d80
Kurt Kline <kurtm.kline@gmail.com>
2020-05-22 01:04:57 +0200

    Added intake and output for raw and clean files

--- cleaning.py
+++ cleaning.py
@@ -1,25 +1,33 @@
 import pandas as pd 
 import numpy as np
 from datetime import date
+from pathlib import Path
+from typing import List, Dict
 
 
-def convert_date(data_date: str, year: int = 2014): 
+def convert_date(data_date: str, year: int = 2014) -> date: 
     month, day = data_date.split('/')
     return date(year, int(month), int(day))
 
-df = pd.read_csv('lpn_fit.csv')
+def get_files(folder: str, regex: str = '*.csv') -> Dict:
+    return {filename:filename.name for filename in Path(folder).glob(regex)}
+
+raw_files = get_files('raw_data')
 columns = ['player', 'date', 'points']
-new_df = pd.DataFrame(columns=columns)
 
-date_cols = df.columns[1:]
-for index, row in df.iterrows():
-    for date_col in date_cols:
-        if not pd.isnull(row[date_col]):
-            clean_date = convert_date(date_col)
-            new_df = new_df.append({'player': row['players'], 
-                                    'date': clean_date, 
-                                    'points': row[date_col]}, ignore_index=True) 
+for file in list(raw_files): 
+    df = pd.read_csv(file)
+    clean_df = pd.DataFrame(columns=columns)
 
-print(new_df)
+    date_cols = df.columns[1:]
+    for index, row in df.iterrows():
+        for date_col in date_cols:
+            if not pd.isnull(row[date_col]):
+                clean_date = convert_date(date_col)
+                clean_df = clean_df.append({'player': row['players'], 
+                                            'date': clean_date, 
+                                            'points': row[date_col]}, 
+                                            ignore_index=True) 
 
+    clean_df.to_csv(f'clean_data/clean_{raw_files[file]}', index=False)
 
