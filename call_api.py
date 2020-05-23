238291d5aa5b4725c110ab80582b4b726cadbc88
Kurt Kline <kurtm.kline@gmail.com>
2020-05-23 15:18:10 +0200

    Added code to call API and load into dataframe

---
+++ call_api.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd 
+import json
+
+response = requests.get("http://127.0.0.1:8000/points/player/Kurt%20Kline")
+response_json = response.json()
+
+# See what it looks like, nicely.
+print(json.dumps(response_json, indent=4))
+
+# Load json into DataFrame
+df = pd.DataFrame(response_json)
+print(df)
